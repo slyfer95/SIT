0c4e714bf68e3a773ab00a9a6e3f05bc21dfcb2a
Waleed Ahmed <44412567+slyfer95@users.noreply.github.com>
2023-11-27 23:22:24 +0500

    Update supabaseClient.jsx

--- src/components/supabaseClient.jsx
+++ src/components/supabaseClient.jsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://joyvqkrrenehkoapnxen.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpveXZxa3JyZW5laGtvYXBueGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcwMTYwNzUsImV4cCI6MjAwMjU5MjA3NX0.92GJS1X-EoBK_mI59FySHBxRGn-FH9G20KRJSkZe2Mg";
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseAnonKey = "YOUR_SUPABASE_KEY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
